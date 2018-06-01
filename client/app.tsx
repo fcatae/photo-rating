e523473448283fb9f06f31e177238521946aaad6
Fabricio Catae <fcatae@microsoft.com>
2018-05-31 21:52:38 -0300

    Add faces

--- client/app.tsx
+++ client/app.tsx
@@ -7,15 +7,40 @@ class App extends React.Component<{},{}> {
    render() {
         var imgList = interop.getImageList();
 
-        return imgList.map( (img, i) =>
-            <ImageBox key={i} url={img} onClick={()=>alert(i)} />
-        );
+        return <div className="panelbox">
+                    <PanelFaces/>
+                    {imgList.map( (img, i) =>
+                        <ImageBox key={i} url={img} onClick={()=>alert(i)} />
+                    )} 
+                </div>
+        
    }
 }
 
 interface ImageBoxProps {
     url: string;
     onClick: any;
+}
+
+class PanelFaces extends React.Component<{},{}> {
+    render() {        
+        var position = {
+            right: 0,
+            bottom: 0
+        };
+        return <div className="panel-faces" style={position}><TagFace id="bad" image="bad.png"/><TagFace id="good" image="good.png"/></div>
+    }
+}
+
+interface TagFaceProps {
+    id: string;
+    image: string;
+}
+
+class TagFace extends React.Component<TagFaceProps,{}> {
+    render() {
+        return <div className="facebox"><img className="facebox-img" src={`resources/${this.props.image}`} onClick={()=>alert(this.props.id)} /></div>;
+    }
 }
 
 class ImageBox extends React.Component<ImageBoxProps,{}> {
